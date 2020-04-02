7bdda2830db7f8fa3a07de30b89c5de748ad4899
Alan Skaev <askaev@rccl.com>
2020-04-02 16:58:28 -0400

    Game implementation

--- core/src/main/java/com/learn/GameImpl.java
+++ core/src/main/java/com/learn/GameImpl.java
@@ -3,12 +3,10 @@ package com.learn;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-@Component
 public class GameImpl implements Game {
 
     // == coonstants ==
