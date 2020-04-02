d06dfd69f87fe4cd079457e6e964cc268744effa
Alan Skaev <askaev@rccl.com>
2020-04-02 13:24:16 -0400

    Main class

---
+++ core/src/main/java/com/learn/Main.java
@@ -0,0 +1,12 @@
+package com.learn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
+
+  public static void main(String[] args) {
+    log.info("Guess the number game");
+  }
+}
