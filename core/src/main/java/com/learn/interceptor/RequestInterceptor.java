9061adf67d29b880d2ff61f80522839d0e84af3c
Alan Skaev <askaev@rccl.com>
2020-04-06 19:09:36 -0400

    Locale Change interceptor

---
+++ core/src/main/java/com/learn/interceptor/RequestInterceptor.java
@@ -0,0 +1,33 @@
+package com.learn.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Slf4j
+public class RequestInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("preHandle method call, handler = {}", handler);
+        log.debug("url = {}", request.getRequestURL());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.debug("postHandle method call, handler = {}", handler);
+        log.debug("url = {}", request.getRequestURL());
+        log.debug("Model = {}", modelAndView.getModel());
+        log.debug("View = {}", modelAndView.getView());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.debug("afterCompletion method call, handler = {}", handler);
+        log.debug("url = {}", request.getRequestURL());
+    }
+}
