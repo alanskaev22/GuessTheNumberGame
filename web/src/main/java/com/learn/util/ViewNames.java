fd162fc31603f7ec8089fe7108b39a0280c13a45
Alan Skaev <askaev@rccl.com>
2020-04-05 20:37:44 -0400

    Spring boot and Web module implementation

---
+++ web/src/main/java/com/learn/util/ViewNames.java
@@ -0,0 +1,11 @@
+package com.learn.util;
+
+public final class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+
+    // == constructor ==
+    private ViewNames() {
+    }
+}
