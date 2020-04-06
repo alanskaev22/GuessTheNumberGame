39d139dbb2bc6674d334fc47089c8d8468ba92a1
Alan Skaev <askaev@rccl.com>
2020-04-06 18:20:11 -0400

    Mappings

---
+++ web/src/main/java/com/learn/util/Mappings.java
@@ -0,0 +1,14 @@
+package com.learn.util;
+
+public final class Mappings {
+
+
+    public static final String PLAY = "play";
+    public static final String REDIRECT_PLAY = "redirect:/" + PLAY;
+    public static final String RESTART = "restart";
+    public static final String HOME = "/";
+
+
+    private Mappings() {
+    }
+}
