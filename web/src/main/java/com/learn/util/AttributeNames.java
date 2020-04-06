f0f2c6a8ca93980453ccfab9c12514a4011f5ab9
Alan Skaev <askaev@rccl.com>
2020-04-06 18:20:33 -0400

    Attribute names

---
+++ web/src/main/java/com/learn/util/AttributeNames.java
@@ -0,0 +1,10 @@
+package com.learn.util;
+
+public final class AttributeNames {
+
+    public static final String MAIN_MESSAGE = "mainMessage";
+    public static final String RESULT_MESSAGE = "resultMessage";
+
+    private AttributeNames() {
+    }
+}
